d809b15e7bbdbbb63e5fea416e1591a5755cd0e6
JonnyRoller23 <maxim55miui11@gmail.com>
2022-01-28 01:15:09 +0300

    olivelite: HavocOS here!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The HavocOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_olivelite.mk
+    $(LOCAL_DIR)/havoc_olivelite.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_olivelite-user \
-    lineage_olivelite-userdebug \
-    lineage_olivelite-eng
+    havoc_olivelite-user \
+    havoc_olivelite-userdebug \
+    havoc_olivelite-eng
--- havoc_olivelite.mk
+++ havoc_olivelite.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The HavocOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,15 +9,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common HavocOS stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Inherit from olivelite device
 $(call inherit-product, device/xiaomi/olivelite/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := olivelite
-PRODUCT_NAME := lineage_olivelite
+PRODUCT_NAME := havoc_olivelite
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 8A
@@ -25,7 +25,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
 
 # Some build flags
-TARGET_BOOT_ANIMATION_RES := 720
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK := true
 TARGET_USES_BLUR := true
 TARGET_USES_AOSP_CHARGER := true
