987f2718aa96fdea7b42f35d0784ad3f16fc0a5e
JonnyRoller23 <maxim55miui11@gmail.com>
2022-01-28 00:45:00 +0300

    olivelite: NusantaraOS here!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The NusantaraOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_olivelite.mk
+    $(LOCAL_DIR)/nad_olivelite.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_olivelite-user \
-    lineage_olivelite-userdebug \
-    lineage_olivelite-eng
+    nad_olivelite-user \
+    nad_olivelite-userdebug \
+    nad_olivelite-eng
