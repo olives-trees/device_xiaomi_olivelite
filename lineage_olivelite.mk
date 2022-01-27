EXOdev-698/device_xiaomi_olivelite-

olivelite: Inherit from common tree
Change-Id: I9eb2ca59312c55d494cb18557689898d9cdf5f62

lineage_olivelite.mk
@@ -1,0 +1,30 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from olivelite device
+$(call inherit-product, device/xiaomi/olivelite/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := olivelite
+PRODUCT_NAME := lineage_olivelite
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 8A
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "Xiaomi/olivelite/olivelite:10/QKQ1.191014.001/V12.0.2.0.QCNMIXM:user/release-keys"

olives-trees/device_xiaomi_olivelite

lineage_olivelite.mk
@@ -1,0 +1,30 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from olivelite device
+$(call inherit-product, device/xiaomi/olivelite/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := olivelite
+PRODUCT_NAME := lineage_olivelite
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 8A
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "Xiaomi/olivelite/olivelite:10/QKQ1.191014.001/V12.0.2.0.QCMMIXM:user/release-keys"