57293b57dbccba85082818c45d43ab392438700f
JonnyRoller23 <maxim55miui11@gmail.com>
2022-01-28 01:08:04 +0300

    olivelite: CrDroidOS here!

--- lineage_olivelite.mk
+++ lineage_olivelite.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The CrDroidOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common CrDroidOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from olivelite device
