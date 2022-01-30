1f33ddc0ac4edb20d210a1299b5d2e8bd57c8883
JonnyRoller23 <maxim55miui11@gmail.com>
2022-01-30 20:24:24 +0300

    olivelite: SyberiaOS here!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The SyberiaOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_olivelite.mk
+    $(LOCAL_DIR)/syberia_olivelite.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_olivelite-user \
-    lineage_olivelite-userdebug \
-    lineage_olivelite-eng
+    syberia_olivelite-user \
+    syberia_olivelite-userdebug \
+    syberia_olivelite-eng
