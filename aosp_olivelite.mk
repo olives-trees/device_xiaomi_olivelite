1dd9a3a1df56e425d25a6e91443433ef20d1dc68
JonnyRoller_23 <71149028+JonnyRoller23@users.noreply.github.com>
2022-01-31 05:55:39 +0300

    Update aosp_olivelite.mk

--- aosp_olivelite.mk
+++ aosp_olivelite.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common PixelExperienceOS stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-e
+
 # Inherit from olivelite device
 $(call inherit-product, device/xiaomi/olivelite/device.mk)
 
